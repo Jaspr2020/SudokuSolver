f2ff862d60e523ba5a21f91b650ed1e6fd056150
Jaspr2020 <jsr5577@gmail.com>
2022-10-14 00:19:31 -0400

    Finished Assignment
    The basic coding assignment is finished but I still plan to earn the extra credit by improving the algorithm. The improvements will entail using the most constrained variable and least constraining
    value for the recursive solve function.

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,33 +1,97 @@
-def input_to_board(input_string):
+def string_to_board(input_string):
     # Create board
     board = [[0 for i in range(9)] for j in range(9)]
-    # For every row and column in the board
+
     for row in range(9):
         for col in range(9):
-            # Set the value at (row, col) to the value of the input string as a number
+            # Convert character to integer ('.' becomes 0)
             board[row][col] = 0 if input_string[row * 9 + col] == '.' else int(input_string[row * 9 + col])
+            
     return board
-        
-    
+
+
 def print_board(board):
-    # Output string
     output = ""
-    # For every row and column in the board
+    
     for row in range(9):
-        # Add a spacing line every 3 rows
+        # Horrizontal line every 3 rows
         if row != 0 and row % 3 == 0:
             output += "---------------------\n"
         for col in range(9):
-            # Add spaces between numbers and vertical bars between every 3 numbers
-            if col != 0:
-                output += " "
-                if col % 3 == 0:
-                    output += "| "
-            output +=  str(board[row][col])
-        output += "\n"
+            # Vertical bars between every 3rd column
+            if col != 0 and col % 3 == 0:
+                output += "| "
+            # Value at index
+            output += str(board[row][col]) + " "
+        if row != 8:
+            output += "\n"
+        
     print(output)
+    
+def possible_values(position, board):
+    # Return empty list if the value is known
+    if board[position[0]][position[1]] != 0:
+        return []
+    
+    # List of possible values
+    values = [i for i in range(1, 10)]
+    
+    # Check row for invalid values
+    for i in range(9):
+        if board[position[0]][i] in values and i != position[1]:
+            values.remove(board[position[0]][i])
+            
+    # Check column for invalid values
+    for i in range(9):
+        if board[i][position[1]] in values and i != position[0]:
+            values.remove(board[i][position[1]])
+            
+    # Check square for invalid values
+    square_x = int(position[1] / 3) * 3
+    square_y = int(position[0] / 3) * 3
+    for i in range(square_y, square_y + 3):
+        for j in range(square_x, square_x + 3):
+            if board[i][j] in values and (i, j) != position:
+                values.remove(board[i][j])
+                
+    # Return the list of remaining values
+    return values
 
-# input_string = input("Enter an unsolved Sudoku Grid: ")
-input_string = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
-board = input_to_board(input_string)
-print_board(board)
+def find_empty(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                return (row, col)
+    # If there are no unsolved spaces
+    return None
+
+def solve(board):
+    # Get the row and column of the next value to set
+    position = find_empty(board)
+    
+    # Solution found
+    if not position:
+        return True
+    
+    # Try all possible at position
+    for n in possible_values(position, board):
+        board[position[0]][position[1]] = n
+        
+        # If solved, stop searching for solutions
+        if solve(board) == True:
+            return True
+        
+        # Undo the move
+        board[position[0]][position[1]] = 0
+    
+    # No solution found
+    return False
+
+def main():
+    input_string = input("Enter an unsolved Sudoku Grid: ")
+    board = string_to_board(input_string)
+    solve(board)
+    print_board(board)
+    
+if __name__ == "__main__":
+    main()
